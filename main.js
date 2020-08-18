3f9a251e09b03b849ce1f7f2c589545cbb9f09f5
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-08-18 21:22:21 +0530

    updated

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 new Vue({
-    el:'#app',
-    data() {
-        return {
-            
-        }
+  el: "#app",
+  data() {
+    return {
+      redirect: false,
+    };
+  },
+  methods: {
+    githubClicked() {
+      document.getElementById("backdrop-id").style.display = "block";
+      document.getElementById("popup-window-id").style.display = "block";
     },
-    methods: {
-        githubClicked(){
-          window.location.href="https://github.com/ashumsd7"
-        },
-        linkdinClicked(){
-            window.location.href=""
-        },
-        mediumClicked(){
-            window.location.href="https://medium.com/@ashumsd7"
-        },
-        quoraClicked(){
-            window.location.href="https://www.quora.com/profile/आशुतोष-आनन्द-तिवारी-Ashutosh-Anand-Tiwari"
-        },
-        twitterClicked(){
-            window.location.href="https://twitter.com/YourVueJS"
-        },
+    linkdinClicked() {
+      window.location.href = "";
+    },
+    mediumClicked() {
+      window.location.href = "https://medium.com/@ashumsd7";
+    },
+    quoraClicked() {
+      window.location.href =
+        "https://www.quora.com/profile/आशुतोष-आनन्द-तिवारी-Ashutosh-Anand-Tiwari";
+    },
+    twitterClicked() {
+      window.location.href = "https://twitter.com/YourVueJS";
     },
 
-})
+    continueClicked() {
+      this.redirect = true;
+      if (this.redirect) {
+        window.location.href = "https://github.com/ashumsd7";
+      }
+    },
+    noClicked(){
+        this.redirect = false;
+        document.getElementById("backdrop-id").style.display = "none";
+        document.getElementById("popup-window-id").style.display = "none";
+    }
+  },
+});
