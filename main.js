07422b4fcf195ecefc21c83a8f749e501d1f7b20
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-08-18 10:46:08 +0530

    updated

---
+++ main.js
@@ -0,0 +1,26 @@
+new Vue({
+    el:'#app',
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        githubClicked(){
+          window.location.href="https://github.com/ashumsd7"
+        },
+        linkdinClicked(){
+            window.location.href=""
+        },
+        mediumClicked(){
+            window.location.href="https://medium.com/@ashumsd7"
+        },
+        quoraClicked(){
+            window.location.href="https://www.quora.com/profile/आशुतोष-आनन्द-तिवारी-Ashutosh-Anand-Tiwari"
+        },
+        twitterClicked(){
+            window.location.href="https://twitter.com/YourVueJS"
+        },
+    },
+
+})
