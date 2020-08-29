04587a2b7e5e1f7de7b888cf4bc575f34db720b9
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-08-29 21:17:44 +0530

    updated

--- projetcs/project.js
+++ projetcs/project.js
@@ -2,11 +2,18 @@ new Vue({
     el: "#project",
     data() {
       return {
-      name: "price"
+      first_project_title:'COVID19 DASHBOARD',
+      first_project_live_link:'https://nocovid.netlify.app/',
+      first_project_code_link:'',
+      first_project_description:'',
+
       };
     },
     methods: {
-     
+     covidLiveLinkClicked(){
+       window.location.href="https://github.com/ashumsd7/covid"
+    
+     }
      
     },
   });
